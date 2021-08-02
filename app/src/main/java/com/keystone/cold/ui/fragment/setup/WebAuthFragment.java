eaf7102c6138da929b7078fe7513848f72765ce3
final_statics <finalstatics@gmail.com>
2021-08-02 15:35:14 +0800

    refactor WebAuthFragment

--- app/src/main/java/com/keystone/cold/ui/fragment/setup/WebAuthFragment.java
+++ app/src/main/java/com/keystone/cold/ui/fragment/setup/WebAuthFragment.java
@@ -24,13 +24,30 @@ import android.provider.Settings;
 import android.view.View;
 import android.widget.Toast;
 
+import androidx.lifecycle.ViewModelProviders;
+
 import com.keystone.cold.R;
 import com.keystone.cold.databinding.WebAuthBinding;
 import com.keystone.cold.ui.fragment.BaseFragment;
+import com.keystone.cold.ui.fragment.main.scan.scanner.ScanResult;
+import com.keystone.cold.ui.fragment.main.scan.scanner.ScanResultTypes;
+import com.keystone.cold.ui.fragment.main.scan.scanner.ScannerState;
+import com.keystone.cold.ui.fragment.main.scan.scanner.ScannerViewModel;
+import com.keystone.cold.viewmodel.WatchWallet;
+import com.keystone.cold.viewmodel.exceptions.UnknowQrCodeException;
+import com.sparrowwallet.hummingbird.registry.CryptoPSBT;
 import com.yanzhenjie.permission.AndPermission;
 import com.yanzhenjie.permission.Permission;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.spongycastle.util.encoders.Base64;
+import org.spongycastle.util.encoders.Hex;
+
+import java.util.Arrays;
+
 import static com.keystone.cold.Utilities.IS_SETUP_VAULT;
+import static com.keystone.cold.ui.fragment.setup.WebAuthResultFragment.WEB_AUTH_DATA;
 
 public class WebAuthFragment extends BaseFragment<WebAuthBinding> {
     @Override
@@ -53,12 +70,8 @@ public class WebAuthFragment extends BaseFragment<WebAuthBinding> {
         AndPermission.with(this)
                 .permission(Permission.CAMERA, Permission.READ_EXTERNAL_STORAGE)
                 .onGranted(permissions -> {
-                    long startTime = System.currentTimeMillis();
-                    Bundle bundle = new Bundle();
-                    bundle.putLong("starttime", startTime);
-                    bundle.putBoolean(IS_SETUP_VAULT, true);
-                    bundle.putString("purpose", "webAuth");
-                    navigate(R.id.webAuth_to_QRCodeScan, bundle);
+                    initScanResult();
+                    navigate(R.id.action_to_scanner);
                 })
                 .onDenied(permissions -> {
                     Uri packageURI = Uri.parse("package:" + mActivity.getPackageName());
@@ -73,5 +86,43 @@ public class WebAuthFragment extends BaseFragment<WebAuthBinding> {
         Bundle bundle = new Bundle();
         bundle.putBoolean(IS_SETUP_VAULT, true);
         navigate(R.id.action_webAuthFragment_to_setPasswordFragment, bundle);
+    }
+
+    private void initScanResult() {
+        ViewModelProviders.of(mActivity).get(ScannerViewModel.class)
+                .setState(new ScannerState(Arrays.asList(ScanResultTypes.PLAIN_TEXT,
+                        ScanResultTypes.UR_BYTES, ScanResultTypes.UR_CRYPTO_PSBT)) {
+                    @Override
+                    public void handleScanResult(ScanResult result) throws Exception {
+                        if (result.getType().equals(ScanResultTypes.PLAIN_TEXT)) {
+                            alert(getString(R.string.invalid_webauth_qrcode_hint));
+                        } else if (result.getType().equals(ScanResultTypes.UR_BYTES)) {
+                            byte[] bytes = (byte[]) result.resolve();
+                            String hex = Hex.toHexString(bytes);
+                            JSONObject data = new JSONObject(hex);
+                            if (data.optString("type").equals("webAuth")) {
+                                String webAuthData = data.getString("data");
+                                Bundle bundle = new Bundle();
+                                bundle.putString(WEB_AUTH_DATA, webAuthData);
+                                bundle.putBoolean(IS_SETUP_VAULT, true);
+                                mFragment.navigate(R.id.action_to_webAuthResultFragment, bundle);
+                            }
+                        } else if (result.getType().equals(ScanResultTypes.UR_CRYPTO_PSBT)) {
+                            WatchWallet watchWallet = WatchWallet.getWatchWallet(mActivity.getApplication());
+                            if (watchWallet.supportBc32QrCode() && watchWallet.supportPsbt()) {
+                                CryptoPSBT cryptoPSBT = (CryptoPSBT) result.resolve();
+                                byte[] bytes = cryptoPSBT.getPsbt();
+                                String psbtB64 = Base64.toBase64String(bytes);
+                                Bundle bundle = new Bundle();
+                                bundle.putString("psbt_base64", psbtB64);
+                                mFragment.navigate(R.id.action_to_psbtTxConfirmFragment, bundle);
+                            } else {
+                                mFragment.alert(getString(R.string.unsupported_qrcode));
+                            }
+                        } else {
+                            throw new UnknowQrCodeException("not support bc32 qrcode in current wallet mode");
+                        }
+                    }
+                });
     }
 }
